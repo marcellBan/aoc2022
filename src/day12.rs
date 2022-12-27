45426155cd418ad4e426ae439f3bfedd3a71c558
Marcell Bán <afga93+cc@gmail.com>
2022-12-27 13:24:39 +0100

    day12: working backtrack but does not find the shortest path

--- src/day12.rs
+++ src/day12.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashSet, io};
+use std::{
+    cell::RefCell,
+    collections::{HashSet, VecDeque},
+    io,
+};
 
 #[derive(PartialEq, Eq, Clone, Hash, Debug)]
 struct Pos {
@@ -21,71 +25,116 @@ impl From<(usize, usize)> for Pos {
     }
 }
 
-fn generate_valid_steps(pos: &Pos, row_count: usize, col_count: usize) -> Vec<Pos> {
+struct TracePos {
+    pos: Pos,
+    possible_steps: RefCell<Vec<Pos>>,
+}
+
+struct Map {
+    data: Vec<Vec<u32>>,
+    start: Pos,
+    end: Pos,
+}
+
+impl From<&Vec<String>> for Map {
+    fn from(value: &Vec<String>) -> Self {
+        let mut height_map = value
+            .iter()
+            .map(|l| l.chars().map(char::into).collect::<Vec<u32>>())
+            .collect::<Vec<_>>();
+
+        let mut end = Pos::default();
+        let mut current = Pos::default();
+
+        for row in 0..height_map.len() {
+            for col in 0..height_map[0].len() {
+                match char::from_u32(height_map[row][col]).unwrap() {
+                    'S' => {
+                        current = Pos::from((col, row));
+                        height_map[row][col] = 'a' as u32;
+                    }
+                    'E' => {
+                        end = Pos::from((col, row));
+                        height_map[row][col] = 'z' as u32;
+                    }
+                    _ => (),
+                }
+            }
+        }
+        Map {
+            data: height_map,
+            start: current,
+            end: end,
+        }
+    }
+}
+
+impl Map {
+    fn at(self: &Self, pos: &Pos) -> u32 {
+        self.data[pos.y][pos.x]
+    }
+
+    fn size(self: &Self) -> (usize, usize) {
+        (self.data.len(), self.data[0].len())
+    }
+}
+
+fn generate_valid_steps(pos: &Pos, map: &Map, visited: &HashSet<Pos>) -> Vec<Pos> {
     let mut steps = Vec::new();
+    let (rows, cols) = map.size();
     if pos.x > 0 {
         steps.push(Pos::from((pos.x - 1, pos.y)));
     }
-    if pos.x < col_count - 1 {
+    if pos.x < cols - 1 {
         steps.push(Pos::from((pos.x + 1, pos.y)));
     }
     if pos.y > 0 {
         steps.push(Pos::from((pos.x, pos.y - 1)));
     }
-    if pos.y < row_count - 1 {
+    if pos.y < rows - 1 {
         steps.push(Pos::from((pos.x, pos.y + 1)));
     }
-    println!("Possible: {:?}", steps);
-    steps
-}
-
-fn find_best_step(pos: &Pos, map: &Vec<Vec<u32>>) -> Pos {
-    generate_valid_steps(pos, map.len(), map[0].len())
+    steps = steps
         .iter()
-        .filter(|p| map[p.y][p.x] <= map[pos.y][pos.x] + 1)
-        .max_by_key(|p| map[p.y][p.x])
-        .unwrap()
-        .clone()
+        .filter(|p| map.at(p) <= map.at(pos) + 1 && !visited.contains(p))
+        .map(|p| p.clone())
+        .collect::<Vec<_>>();
+    steps.sort_by_key(|p| map.at(p));
+    steps
 }
 
 use crate::input_reader;
 pub fn solve() -> io::Result<()> {
-    let lines = input_reader::read_input("input/test.in")?;
+    let lines = input_reader::read_input("input/day12.in")?;
 
-    let mut height_map = lines
-        .iter()
-        .map(|l| l.chars().map(char::into).collect::<Vec<u32>>())
-        .collect::<Vec<_>>();
+    let map = Map::from(&lines);
+    let mut current_pos = map.start.clone();
 
-    let mut end = Pos::default();
-    let mut current = Pos::default();
+    let mut visited: HashSet<Pos> = HashSet::new();
+    visited.insert(current_pos.clone());
+    let mut trace: VecDeque<TracePos> = VecDeque::new();
+    trace.push_back(TracePos {
+        pos: current_pos.clone(),
+        possible_steps: generate_valid_steps(&current_pos, &map, &visited).into(),
+    });
 
-    for row in 0..height_map.len() {
-        for col in 0..height_map[0].len() {
-            match char::from_u32(height_map[row][col]).unwrap() {
-                'S' => {
-                    current = Pos::from((col, row));
-                    height_map[row][col] = 'a' as u32;
-                }
-                'E' => {
-                    end = Pos::from((col, row));
-                    height_map[row][col] = 'z' as u32;
-                }
-                _ => (),
-            }
-        }
+    while current_pos != map.end && !trace.is_empty() {
+        let maybe_next_pos = trace.back().unwrap().possible_steps.borrow_mut().pop();
+        let next_pos = if let Some(p) = maybe_next_pos {
+            p
+        } else {
+            trace.pop_back().unwrap();
+            continue;
+        };
+        trace.push_back(TracePos {
+            pos: next_pos.clone(),
+            possible_steps: generate_valid_steps(&next_pos, &map, &visited).into(),
+        });
+        visited.insert(next_pos.clone());
+        current_pos = next_pos;
     }
 
-    let mut steps: HashSet<Pos> = HashSet::new();
-
-    while current != end {
-        steps.insert(current.clone());
-        current = find_best_step(&current, &height_map);
-        println!("Updated: {:?}", current);
-        if steps.contains(&current) {
-            break;
-        }
-    }
+    println!("Took {} steps", trace.len() - 1);
 
     Ok(())
 }
