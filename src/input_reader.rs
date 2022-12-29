8de01c884d1f74d4a42132f11a5f5bebdd09c839
Marcell Bán <afga93+cc@gmail.com>
2022-12-29 11:50:24 +0100

    launch configs and input reader env aware

--- src/input_reader.rs
+++ src/input_reader.rs
@@ -1,7 +1,9 @@
-use std::fs;
-use std::io;
+use std::{env, fs, io};
 
 pub fn read_input(file_path: &str) -> io::Result<Vec<String>> {
-    let content = fs::read_to_string(file_path)?;
+    let content = fs::read_to_string(match env::var("AOC_TEST") {
+        Ok(_) => "input/test.in",
+        _ => file_path,
+    })?;
     Ok(content.split("\n").map(str::to_owned).collect::<Vec<_>>())
 }
